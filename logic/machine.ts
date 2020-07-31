de83a595d1158e4139adfa9a6f363eb6f5d810c0
Christian Bielert <cib123@googlemail.com>
2020-07-31 17:35:49 +0200

    WIP

--- logic/machine.ts
+++ logic/machine.ts
@@ -3,3 +3,36 @@ class IncrementalStatementMachine {
     constructions: Construction[]
     blocks: Block[]
 }
+
+interface DataType {
+    symbol: string
+}
+
+abstract class ValueExpression {
+    readonly type: string
+}
+
+class BaseConstructorExpression {
+    readonly type = 'BaseConstructor'
+    symbol: string
+}
+
+class CompositeConstructorExpression {
+    readonly type = 'CompositeConstructor'
+    symbol: string
+    parameters: ValueExpression[]
+}
+
+class Relation {
+    symbol: string
+    parameters: DataType[]
+
+    arity(): number {
+        return this.parameters.length
+    }
+}
+
+interface Statement {
+    relation: Relation
+    arguments: ValueExpression[]
+}
---
+++ proofs.md
@@ -0,0 +1,5 @@
+# Proofs
+
+- Primarily driven by human interaction
+- Statement to prove set by human
+- 
